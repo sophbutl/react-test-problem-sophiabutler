1777871a5fdec43d684948182e26d244c36066b3
sophbutl <127991524+sophbutl@users.noreply.github.com>
2023-03-15 16:24:42 -0400

    Update App.stories.js

--- src/App.stories.js
+++ src/App.stories.js
@@ -1,2 +1,13 @@
-// Stories for testing `App` go here
-// See https://storybook.js.org/docs/react/writing-stories/introduction#how-to-write-stories
+import React from 'react';
+
+import App from "./App";
+
+export default {
+  title: "App",
+  component: App,
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  thanksMessage: false,
+};
